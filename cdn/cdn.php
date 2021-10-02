539f79b0b71bc6b1ef96ff6239789b406c512b9f
Elizabeth21-Zabaleta <88984576+Elizabeth21-Zabaleta@users.noreply.github.com>
2021-10-02 08:28:45 -0600

    Add files via upload

---
+++ cdn/cdn.php
@@ -0,0 +1,42 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/normalize.css">
+	<link rel="stylesheet" href="./css/sweetalert2.css">
+	<link rel="stylesheet" href="./css/sweetalert2_1.css">
+	<link rel="stylesheet" href="./css/material.min.css">
+	<link rel="stylesheet" href="./css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="./css/material-design-iconic-font.min_1.css">
+	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar.css">
+	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar_1.css">
+	<link rel="stylesheet" href="./css/main_1.css">
+	<link rel="stylesheet" href="./css/main.css">
+	<link href="./fonts/css/fontawesome.css" rel="stylesheet">
+    <link href="./fonts/css/brands.css" rel="stylesheet">
+    <link href="./fonts/css/solid.css" rel="stylesheet">
+     <link rel="shortcut icon" type="../image/x-icon" href="./assets/img/icono.png">
+	</head>
+
+
+<footer>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="./js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="./js/material.min.js" ></script>
+	<script src="./js/material.min_1.js" ></script>
+	<script src="./js/sweetalert2.min.js" ></script>
+	<script src="./js/sweetalert2.min_1.js" ></script>
+	<script src="./js/jquery.mCustomScrollbar.concat.min.js" ></script>
+	<script src="./js/jquery.mCustomScrollbar.concat.min_1.js" ></script>
+	<script src="./js/main.js" ></script>
+	<script src="./js/main_1.js" ></script>
+	<script src="./js/ripples.min.js"></script>
+	 <script src="./js/bootstrap.min.js"></script>
+	 <script src="./js/jquery-3.1.1.min.js"></script>
+	 <script defer src="./fonts/js/brands.js"></script>
+  	<script defer src="./fonts/js/solid.js"></script>
+  	<script defer src="./fonts/js/fontawesome.js"></script>
+    <script>
+        $.material.init();
+    </script>
+</footer>
