cb47b9827b92483adfbc5ad0a961e091936b0f34
Elizabeth21-Zabaleta <88984576+Elizabeth21-Zabaleta@users.noreply.github.com>
2021-10-31 11:53:11 -0600

    Add files via upload

--- cdn/cdn.php
+++ cdn/cdn.php
@@ -2,7 +2,6 @@
 <html lang="es">
 <head>
 		<title> Sistema de Gestión de Libros Parroquiales</title>
-	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,11 +13,13 @@
 	<link rel="stylesheet" href="./css/material-design-iconic-font.min_1.css">
 	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar.css">
 	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar_1.css">
+		<link rel="stylesheet" href="./css/main.css">
 	<link rel="stylesheet" href="./css/main_1.css">
-	<link rel="stylesheet" href="./css/main.css">
+
 	<link href="./fonts/css/fontawesome.css" rel="stylesheet">
     <link href="./fonts/css/brands.css" rel="stylesheet">
     <link href="./fonts/css/solid.css" rel="stylesheet">
      <link rel="shortcut icon" type="../image/x-icon" href="./assets/img/avatar.jpeg">
+
 	</head>
 
---
+++ cdn/links.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title> Sistema de Gestión de Libros Parroquiales</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+           <link href="./fonts/css/fontawesome.css" rel="stylesheet">
+            <link href="./fonts/css/brands.css" rel="stylesheet">
+        <link href="./fonts/css/solid.css" rel="stylesheet">
+            <link rel="stylesheet" href="./css/main.css">
+        <link rel="shortcut icon" type="../image/x-icon" href="./assets/img/avatar.jpeg">
