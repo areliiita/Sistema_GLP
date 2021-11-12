f7acf4526aaab3cff8b53d64e238732f21738a1c
Elizabeth21-Zabaleta <88984576+Elizabeth21-Zabaleta@users.noreply.github.com>
2021-11-12 17:14:51 -0600

    Add files via upload
    Arreglar los parrafos.

--- views/administrador/acta_bautismo.php
+++ views/administrador/acta_bautismo.php
@@ -1,3 +1,4 @@
+
 <style>
      #printable { display: none; }
 
@@ -26,7 +27,31 @@
 
 <div class="mdl-grid">
 <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet">
-<a  href="javascript:Imprimir_Contenido('acta')"><button type="submit" class="btn btn bg-primary btn-raised btn-md" style="color:white;"><i class="fas fa-print"></i>IMPRIMIR</button></a>
+<a  href="javascript:Imprimir_Contenido1('acta_bautismo')"><button type="submit" class="btn btn bg-primary btn-raised btn-md" style="color:white;"><i class="fas fa-print"></i>IMPRIMIR</button></a>
+    </div>
+     </div>
+<?php endif?>
+
+<script>
+    $(document).ready(function() {
+        $("#asignar-v").click(function(event) {
+            var nombrev = $("#nombre-v").val();
+            alert(nombrev);
+            event.preventDefault();
+        });
+    });
+</script>
+<div style="margin-left: 10px;margin-bottom: 15px;">
+    <form method="post">
+        <input type="text" class="form-control" placeholder="Nombre Vicario" name="nombre_v" required="ON" style="width:300px">
+        <button class="btn btn-success" name="nombreVicario">Asignar Vicario a Imprimir en Acta</button>
+    </form>
+</div>
+<?php if (isset($_POST['nombreVicario'])):?>
+
+<div class="mdl-grid">
+<div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet">
+<a  href="javascript:Imprimir_Contenido('acta_bautismo')"><button type="submit" class="btn btn bg-primary btn-raised btn-md" style="color:white;"><i class="fas fa-print"></i> IMPRIMIR</button></a>
     </div>
      </div>
 <?php endif?>
@@ -34,44 +59,40 @@
 
 
 
-<div id="acta">
+<div id="acta_bautismo">
 <div  style="background-color:#E6E6FA">
-    <img style="float:left;margin-bottom:15px;margin-left: 40px; margin-right: 40px;white-space:pre;" src="./views/administrador/logo/logo.png" width="150px" alt="Logo"> &nbsp;
+    <img style="float:left;margin-bottom:0px; margin-top: 10px;margin-left: 40px; margin-right: 40px;white-space:pre;" src="./views/administrador/logo/logo.png" width="130px" alt="Logo"> &nbsp;
     <p>
-    	<br>
-    	<br>
-    	<br>
-    	<br>
-    <h2 style="margin-top:50px;margin-bottom:50px;text-align:center;"><b> FE DE BAUTISMO</b></h2>
+    <h2 style="margin-top:50px;margin-bottom:60px;text-align:justify;"><b> FE DE BAUTISMO</b></h2>
     </p>
-
-    <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-     Partida de bautismo de: <b><?php echo $bautismo->nombre_bautizado; ?></b>.</p>
-
-    <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;"> El infraescrito  Pbro. <b>  <?php
+    <p style="text-align:justify;margin-left:50px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+     Partida de Bautismo de: <b><?php echo $bautismo->nombre_bautizado; ?></b>.<br>
+        El infrascrito <b>
+     <?php
             if (isset($_POST['nombreParroco']))
             {
-                echo $_POST['nombre_p']."<br>";
+                echo "Pbro:". $_POST['nombre_p']."<br>";
             }
-        ?></b></p>
-
-        <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;"> De la parroquia <b><?php echo $bautismo->nombre_parroquia; ?></b>, Ilobasco,Cabañas certifica que:
-
-          <br>
-        <br>
-         <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;"> En la parroquia <b><?php echo $bautismo->nombre_parroquia; ?></b>, Ilobasco a <?php echo $bautismo->fecha_bautismo; ?>. Recibió El Sacramento del Bautismo: <b><?php echo $bautismo->nombre_bautizado; ?></b>, que nació el <?php echo $bautismo->fecha_nacimiento; ?>, en el  <?php echo $bautismo->nombre_hospitaldenacimiento; ?>, siendo <?php echo $bautismo->hijo; ?> de <?php echo $bautismo->nombre_padre; ?> y de <?php echo $bautismo->nombre_madre; ?>.</p>
+        ?>
+         <?php
+            if (isset($_POST['nombreVicario']))
+            {
+                echo "Vicario: ". $_POST['nombre_v']."<br>";
+            }
+        ?>
+        </u></b>
+     De la Parroquia <b><u><?php echo $bautismo->nombre_parroquia; ?></b>, Ilobasco,Cabañas</u> certifica que:
+    </p>
+         <p style="text-align:justify;margin-left:50px;margin-right:50px;line-height: 20pt; font-size: 18px;"> En la Parroquia <b><?php echo $bautismo->nombre_parroquia; ?></b>, Ilobasco a
+<?php echo $bautismo->fecha_bautismo; ?>. Recibió El Sacramento del Bautismo: <b><?php echo $bautismo->nombre_bautizado; ?></b>, que nació el <?php echo $bautismo->fecha_nacimiento; ?>, en el  <?php echo $bautismo->nombre_hospitaldenacimiento; ?>, siendo <?php echo $bautismo->hijo; ?> de <?php echo $bautismo->nombre_padre; ?> y de <?php echo $bautismo->nombre_madre; ?>.
          <br>
-         <br>
+     <b><u>Padrinos: </b><?php echo $bautismo->nombre_padrino; ?>, <?php echo $bautismo->nombre_madrina; ?>  <?php echo $bautismo->nombre_padrino3;?>.</u>
+     <br>
+     <b><u>Ministro: </b>Pbro.<?php echo $bautismo->nombre_ministro; ?></u>.</p>
 
-         <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-     <b>Padrinos: </b><?php echo $bautismo->nombre_padrino; ?>, <?php echo $bautismo->nombre_madrina; ?>  <?php echo $bautismo->nombre_padrino3;?>.</p>
-     <br>
-     <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-     <b>Ministro: </b><?php echo $bautismo->nombre_ministro; ?>.</p>
-     <br>
-     <br>
-     <p style="text-align:justify;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-    La presente es copia fiel del  originario, la que  me remitio y la expide en la Oficina Paroquial  el dia.
+
+     <p style="text-align:justify;margin-left:50px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+    La presente es copia fiel del  original, la que  me remito y la expido en la Oficina Paroquial  el dia
 
 <?php
 date_default_timezone_set('America/El_Salvador');
@@ -79,64 +100,68 @@ $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Domingo
 $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 
 echo $dias[date('w')]." ".date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ;
-?>
+?>.<br>
 </p>
-     <br>
-     <br>
-     <br>
-     <br>
 
-
- <p style="text-align:center;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">Pbro.  <?php
+ <p style="text-align:center;margin-left:50px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+    <?php
             if (isset($_POST['nombreParroco']))
             {
-                echo $_POST['nombre_p']."<br>";
+                echo "Pbro.".$_POST['nombre_p']."<br> Párroco.";
             }
-        ?></p>
-         <p style="text-align:center;margin-left:80px;margin-right:50px;line-height: 5pt; font-size: 20px;">
-            Parroco.
+        ?>
+    <?php
+            if (isset($_POST['nombreVicario']))
+            {
+                echo $_POST['nombre_v']."<br> Vicario. ". "<br>";
+            }
+        ?>
+
         </p>
-         <br><br>
 
 
-      <p style="text-align:justify;margin-left:100px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-          <b>AUTENTICIDAD:<br></b>Se certifica que el sello de la Paroquia  de_______________________________________________________
+
+      <p style="text-align:justify;margin-left:70px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+          <b>AUTENTICIDAD:<br></b>Se certifica que el sello de la Paroquia  de_________________________________
+            <br>
+          Y la firma del Párroco P.__________________________________son auténticos.
+          <br>
+          SELLO
         </p>
-         <p style="text-align:justify;margin-left:100px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-          Y la firma del Párroco P._________________________________________________________son auténticos.</p> <br><br>
-          <p style="text-align:justify;margin-left:100px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-          SELLO__________________________________________________________________________________<br>
-          <p style="text-align:justify;margin-left:800px;margin-right:50px;line-height: 20pt; font-size: 20px;">VICARIO GENERAL
+        <p style="text-align:right;margin-left:70px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+         VICARIO GENERAL
         </p>
-         <br>
-         <br>
-         <br>
-         <br>
-         <p style="text-align:justify;margin-left:100px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-        San Vicente, a_____________________ de _____________________ del Dos mil _____________________</p>.
-    </p>
-    <br>
-    <br>
-    <div>
-        <hr>
-           <p style="text-align:right;margin-left:80px;margin-right:50px;line-height: 20pt; font-size: 20px;">
-         <small>  <i>Parroquia San Antonio de Padua <br>
+         <p style="text-align:justify;margin-left:70px;margin-right:50px;line-height: 20pt; font-size: 18Px;">
+         San Vicente, a______________ de ______________ del Dos mil _____________</p>.
+
+           <p style="text-align:right;margin-left:50px;margin-right:50px;line-height: 20pt; font-size: 18px;">
+        <i><small> Parroquia San Antonio de Padua <br>
             Agua Zarca San Antonio km 49 1/2, Carretera a Ilobasco, Cabañas<br>
             E-mail: sanantoniodepaduailob@gmail.com <br>
             Tel.7203-6039
-            </i>
-            </small>
+            </small></i>
          </p>
-         </div>
     </div>
 </div>
 </div>
 
 
 <script language="Javascript">
-  function Imprimir_Contenido(nombre)
+  function Imprimir_Contenido1(nombre)
   {
   var ficha = document.getElementById(nombre);
+  var Ventana_Impresion = window.open(' ', 'popimpr');
+  Ventana_Impresion.document.write( ficha.innerHTML );
+  Ventana_Impresion.document.close();
+  Ventana_Impresion.print( );
+  Ventana_Impresion.close();
+  }
+</script>
+
+<script language="Javascript">
+  function Imprimir_Contenido(nombrev)
+  {
+  var ficha = document.getElementById(nombrev);
   var Ventana_Impresion = window.open(' ', 'popimpr');
   Ventana_Impresion.document.write( ficha.innerHTML );
   Ventana_Impresion.document.close();
